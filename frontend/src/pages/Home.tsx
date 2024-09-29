89470a875413e940d92241f2e30f265d8c73ed0c
Alan K John <itzjustalan@gmail.com>
2024-09-29 23:52:10 +0530

    FIX(app): formatting

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -5,9 +5,15 @@ import serverSvg from "../assets/server.svg";
 export const Home: React.FC = () => {
   return (
     <div>
-      <Typography.Title style={{ textAlign: "center", color: "#112D4E" }}>Pingoh</Typography.Title>
+      <Typography.Title style={{ textAlign: "center", color: "#112D4E" }}>
+        Pingoh
+      </Typography.Title>
 
-      <p style={{ textAlign: "center" }}><Typography.Text>A self contained uptime monitoring tool for homelabs</Typography.Text></p>
+      <p style={{ textAlign: "center" }}>
+        <Typography.Text>
+          A self contained uptime monitoring tool for homelabs
+        </Typography.Text>
+      </p>
 
       <Flex wrap justify="center" align="center" gap="middle">
         <Button type="primary">
@@ -15,12 +21,18 @@ export const Home: React.FC = () => {
             List All tasks
           </Link>
         </Button>
-        <Button type="primary"><Link to={"/tasks/new"} className="[&.active]:font-bold">
-          Create task
-        </Link></Button>
+        <Button type="primary">
+          <Link to={"/tasks/new"} className="[&.active]:font-bold">
+            Create task
+          </Link>
+        </Button>
       </Flex>
-      <img src={serverSvg} alt="" width={"70%"} style={{ margin: "0 auto", display: "block" }} />
-
+      <img
+        src={serverSvg}
+        alt=""
+        width={"70%"}
+        style={{ margin: "0 auto", display: "block" }}
+      />
     </div>
   );
 };
